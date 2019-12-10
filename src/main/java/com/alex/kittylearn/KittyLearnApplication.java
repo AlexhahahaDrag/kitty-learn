39e35c72e23bd163bd3fd78208b3b76158a425d8
AlexhahahaDrag <734663446@qq.com>
2019-12-10 14:46:02 +0800

    create project

---
+++ src/main/java/com/alex/kittylearn/KittyLearnApplication.java
@@ -0,0 +1,13 @@
+package com.alex.kittylearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KittyLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KittyLearnApplication.class, args);
+    }
+
+}
