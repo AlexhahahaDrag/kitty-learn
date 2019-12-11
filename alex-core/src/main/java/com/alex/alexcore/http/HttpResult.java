b837796a0c01a06b8e77dc7ee41704ba4f04c03e
AlexhahahaDrag <734663446@qq.com>
2019-12-11 18:03:57 +0800

    segmentation of engineering

---
+++ alex-core/src/main/java/com/alex/alexcore/http/HttpResult.java
@@ -0,0 +1,72 @@
+package com.alex.alexcore.http;
+
+/**
+ * @Description: HTTP结果封装
+ * @Author:     alex
+ * @CreateDate: 2019/11/13 16:25
+ * @Version:    1.0
+ *
+*/
+public class HttpResult {
+
+    private Integer code = 200;
+
+    private String msg;
+
+    private Object data;
+
+    public static HttpResult error() {
+        return error(HttpStatus.SC_INTERNAL_SERVER_ERROR, "未知异常，请联系管理员");
+    }
+
+    public static HttpResult error(String msg) {
+        return error(HttpStatus.SC_INTERNAL_SERVER_ERROR, msg);
+    }
+
+    public static HttpResult error(int code, String msg) {
+        HttpResult r = new HttpResult();
+        r.setCode(code);
+        r.setData(msg);
+        return r;
+    }
+
+    public static HttpResult ok(String msg) {
+        HttpResult r = new HttpResult();
+        r.setMsg(msg);
+        return r;
+    }
+
+    public static HttpResult ok(Object data) {
+        HttpResult r = new HttpResult();
+        r.setData(data);
+        return r;
+    }
+
+    public static HttpResult ok() {
+        return new HttpResult();
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+}
---
+++ alex-core/src/main/java/com/alex/alexcore/page/MybatisPageHelper.java
@@ -0,0 +1,60 @@
+package com.alex.alexcore.page;
+
+import com.alex.common.util.ReflectionUtils;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+
+import java.util.List;
+
+/**
+ * @Description: Mybatis分页查询助手
+ * @Author:     alex
+ * @CreateDate: 2019/12/10 20:00
+ * @Version:    1.0
+ *
+*/
+public class MybatisPageHelper {
+
+    public static final String findPage = "findPage";
+
+    /**
+     * @Description: 分页查询，约定查询方法名为"findpage";
+     * @Author:      alex
+     * @CreateDate:  2019/12/10 20:02
+     * @param pageRequest 分页请求
+     * @param mapper dao对象 mybatis的mapper
+     * @return
+    */
+    public static PageResult findPage(PageRequest pageRequest, Object mapper) {
+        return findPage(pageRequest, mapper, findPage);
+    }
+
+    /**
+     * @Description: 调用分页插件进行分页查询
+     * @Author:      alex
+     * @CreateDate:  2019/12/10 20:38
+     * @param pageRequest 分页请求
+     * @param pageRequest queryMethodName
+     * @param mapper dao对象 mybatis的mapper
+     * @param args 方法参数
+     * @return
+    */
+    public static PageResult findPage(PageRequest pageRequest, Object mapper, String queryMethodName, Object... args) {
+        int pageNum = pageRequest.getPageNum();
+        int pageSize = pageRequest.getPageSize();
+        PageHelper.startPage(pageNum, pageSize);
+        //利用反射调用查询方法
+        Object result = ReflectionUtils.invoke(mapper, queryMethodName, args);
+        return getPageResult(pageRequest, new PageInfo((List) result));
+    }
+
+    private static PageResult getPageResult(PageRequest pageRequest, PageInfo<?> pageInfo) {
+        PageResult pageResult = new PageResult();
+        pageResult.setPageNum(pageRequest.getPageNum());
+        pageResult.setPageSize(pageRequest.getPageSize());
+        pageResult.setTotalSize(pageInfo.getTotal());
+        pageResult.setTotalPage(pageInfo.getPages());
+        pageResult.setContent(pageInfo.getList());
+        return pageResult;
+    }
+}
